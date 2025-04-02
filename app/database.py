6c55d2faa6df5d05fde2f08a3c939dd9e6b34b6b
vel_kor <viking2412@ukr.net>
2025-04-02 21:34:14 +0200

    fixed caching

--- app/database.py
+++ app/database.py
@@ -1,5 +1,4 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "mysql+aiomysql://myuser:mypassword@localhost:3306/fastapi"
 
--- app/services.py
+++ app/services.py
@@ -27,27 +27,16 @@ async def create_post(db: AsyncSession, user: User, post_contains: str):
     return new_post.id
 
 async def find_post(db: AsyncSession, user: User):
-    posts = await db.execute(select(Post).filter(Post.user_id == user.id))
     if user.id not in cache:
-        cache[user.id] = {}
-    cache[user.id] = posts
-    asyncio.create_task(remove_from_cache(user.id))
-    return cache[user.id].scalars().all()
+        posts = await db.execute(select(Post).filter(Post.user_id == user.id))
+        cache[user.id] = posts.scalars().all()
+        asyncio.create_task(remove_from_cache(user.id))
+    return cache[user.id]
 
-"""def cached_posts(get_posts):
-    def wrapper(user: User):
-        if datetime.timestamp - cache[user.id[1]] <= 300 :
-            return cache[user.id[0]]
-        else:
-            time = datetime.timestamp
-            cache[user.id] = (get_posts(), time)"""
-
+#Removes a cached entry after 'delay' seconds (default: 5 minutes)
 async def remove_from_cache(user_id: int, delay: int = 300):
-    """Removes a cached entry after 'delay' seconds (default: 5 minutes)."""
     await asyncio.sleep(delay)
     if user_id in cache:
         del cache[user_id]
-        """if not cache[user_id]:
-            del cache.keys[user_id]"""
-    #print("CACHE DELETED")
+    print("CACHE DELETED")
     
